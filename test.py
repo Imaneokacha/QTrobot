19859684a583699aa98dd7a3a422a57e3069d870
Imaneokacha <127397518+Imaneokacha@users.noreply.github.com>
2023-05-03 08:36:41 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#!/usr/bin/env python
+import sys
+import rospy
+from qt_robot_interface.srv import *
+from qt_vosk_app.srv import *
+
+
+
+if __name__ == '__main__':
+    
+    # Initialisation du noeud ROS
+    rospy.init_node('qt_robot_speech')
+    rospy.loginfo("Hi") 
+    
+    speechSay = rospy.ServiceProxy('/qt_robot/speech/say', speech_say)
+    recognize = rospy.ServiceProxy('/qt_robot/speech/recognize', speech_recognize)
+    rospy.wait_for_service('/qt_robot/speech/say')
+    rospy.wait_for_service('/qt_robot/speech/recognize')
+
+
+    speechSay("Bonjour! Tu veux faire une recherche? ")
+    resp = recognize("fr_FR", [], 0)
+    print(resp.transcript)
+    rospy.loginfo("I got: %s", resp.transcript)
+    speechSay("Tu as dis: %s " % resp.transcript)
+
+    try:
+        
+        #The code is repeated until the user taps ctrl+C 
+        rospy.spin()
+    except KeyboardInterrupt:
+        pass
+    
+    rospy.loginfo("Finished!")
